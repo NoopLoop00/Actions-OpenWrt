4b536c4a9306db6729c0ad51ef08940566290a41
NoopLoop00 <53203656+NoopLoop00@users.noreply.github.com>
2023-11-28 15:18:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/NoopLoop00/luci-app-vssr.git package/luci-app-vssr
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
