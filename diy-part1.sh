83e58ddb22a857752f73e266fe404b036863d77a
NoopLoop00 <53203656+NoopLoop00@users.noreply.github.com>
2023-11-29 08:45:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git Lienolsopenwrt https://github.com/Lienol/openwrt.git' >>feeds.conf.default
