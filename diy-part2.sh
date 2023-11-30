b8063256352d85981c75db0766c123ef3f742f53
NoopLoop00 <53203656+NoopLoop00@users.noreply.github.com>
2023-11-30 16:25:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-git clone https://github.com/quadhier/coreutils-base64.git package/coreutils-base64
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
